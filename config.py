66ccb33028c54726e7ddf6d680ca3b97f68a466c
Krishna <srikrishnavidyarthi@gmail.com>
2025-04-26 18:05:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "7768725383:AAFOaTqDmuFQw2dAdXlgJH-O760qYfDZyc8"
-auth_users = [5840594311]
+api_id = "24320085"
+api_hash = "a9d8e5464a5ac65180bcb48fda55ec66"
+bot_token = "7598518889:AAE4WT8HWxRuDA_BPSvETUsZ_O7ZipHUWYw"
+auth_users = [7521593568]
